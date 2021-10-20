37c37687ec1a6bd5d331935d3193a57c2ae7fe8e
sommai.k@gmail.com <sommai.k@gmail.com>
2021-10-20 08:44:04 +0000

    support pagination

--- src/main/java/com/example/demo/controller/SubjectController.java
+++ src/main/java/com/example/demo/controller/SubjectController.java
@@ -14,6 +14,8 @@ import com.example.demo.repository.SubjectRepository;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -109,5 +112,16 @@ public class SubjectController {
         }
 
         return resp;
+    }
+
+    @GetMapping("/find-by-code")
+    public List<Subject> findByCode(@RequestParam(value = "code", required = true) String code,
+            @RequestParam(value = "name", required = false, defaultValue = "") String name) {
+        return this.repo.findByCodeOrName(code, name);
+    }
+
+    @GetMapping("/feed")
+    public Page<Subject> feed(Pageable page) {
+        return this.repo.findAll(page);
     }
 }
--- src/main/java/com/example/demo/repository/SubjectRepository.java
+++ src/main/java/com/example/demo/repository/SubjectRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import com.example.demo.entity.Subject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    public List<Subject> findByCodeOrderByCodeDesc(String code);
+
+    public List<Subject> findByCodeOrName(String code, String name);
 
 }
