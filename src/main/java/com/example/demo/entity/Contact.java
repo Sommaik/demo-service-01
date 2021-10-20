70b631b0133fee7ee693d7ba59d45fde7b17a6cd
sommai.k@gmail.com <sommai.k@gmail.com>
2021-10-20 04:58:39 +0000

    add 2 entity

---
+++ src/main/java/com/example/demo/entity/Contact.java
@@ -0,0 +1,120 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_contact")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    int id;
+
+    @ManyToOne()
+    @JoinColumn(name = "con_subject")
+    Subject subject;
+
+    @Column(name = "con_message")
+    String message;
+
+    @Column(name = "con_status")
+    String status;
+
+    @Column(name = "con_comment")
+    String comment;
+
+    @Column(name = "create_date")
+    Date createDate;
+
+    @Column(name = "create_by_ip")
+    String createByIp;
+
+    @Column(name = "update_date")
+    Date updateDate;
+
+    @Column(name = "update_by")
+    String updateBy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateByIp() {
+        return createByIp;
+    }
+
+    public void setCreateByIp(String createByIp) {
+        this.createByIp = createByIp;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+}
---
+++ src/main/java/com/example/demo/entity/Subject.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity()
+@Table(name = "tb_subject")
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "code")
+    String code;
+
+    @Column(name = "name")
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
